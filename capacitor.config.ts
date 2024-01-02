ee1586d6b96d16f5b6bd4e21497284b66670f69c
macortesro <147458427+macortesro@users.noreply.github.com>
2024-01-01 22:42:30 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ph.u2.mcr',
+  appName: 'ph-u2-mcr',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
